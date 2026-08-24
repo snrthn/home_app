aa6fd1b8ef27ff7be052f5252717e617c2daeda7
yanghuning <185471581@qq.com>
2026-08-24 23:39:36 +0800

    test: 核心链路测试用例完成

--- nest/jest.e2e.config.js
+++ nest/jest.e2e.config.js
@@ -2,17 +2,15 @@
 const config = {
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: 'src',
-  testRegex: '.*\\.spec\\.ts$',
-  testPathIgnorePatterns: ['/node_modules/', '\\.e2e\\.spec\\.ts$'],
+  testRegex: '.*\\.e2e\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': ['ts-jest', { tsconfig: 'tsconfig.spec.json' }],
   },
-  // @laoma/shared 是预编译包，直接映射到 dist，避免 jest 走 node_modules 解析的不确定性
   moduleNameMapper: {
     '^@laoma/shared$': '<rootDir>/../../shared/dist/index.js',
   },
-  collectCoverageFrom: ['**/*.(t|j)s'],
   testEnvironment: 'node',
+  testTimeout: 30000,
 };
 
 module.exports = config;
